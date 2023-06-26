0abb23f0568a09a21a2ef01aa2c974f93d5a312c
yuritakagi <takagiyuri0712@gmail.com>
2023-06-26 15:10:30 +0900

    add: データフェッチAPIを使ってリクエスト時に初期画像を取得する

--- random-cat/pages/index.tsx
+++ random-cat/pages/index.tsx
@@ -1,17 +1,27 @@
-import { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
 
-const IndexPage: NextPage = () => {
+// getServerSidePropsから渡されるpropsの型
+type Props = {
+  initialImageUrl: string;
+};
+
+// ページコンポーネントにpropsを受け取る引数を追加する
+// Props型のNextPage変数を宣言
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
   // useStateを使って状態を定義する
-  const [imageUrl, setImageUrl] = useState("");
-  const [loading, setLoading] = useState(true);
+  // 状態変数imageUrl、その状態を更新するための関数setImageUrl
+  // initialImageUrlが初期値としてuseStateに渡され、受け取った初期値をimageUrlとして使用する
+  // useStateは配列を返すため、分割代入を使用してそれぞれの要素を取得
+  const [imageUrl, setImageUrl] = useState(initialImageUrl);
+  const [loading, setLoading] = useState(false);
   // マウント時に画像を読み込む宣言
-  useEffect(() => {
-    fetchImage().then((newImage) => {
-      setImageUrl(newImage.url); // 画像URLの状態を更新する
-      setLoading(false); // ローディング状態を更新する
-    });
-  }, []);
+  // useEffect(() => {
+  //   fetchImage().then((newImage) => {
+  //     setImageUrl(newImage.url); // 画像URLの状態を更新する
+  //     setLoading(false); // ローディング状態を更新する
+  //   });
+  // }, []);
   // ボタンをクリックしたときに画像を読み込む処理
   const handleClick = async () => {
     setLoading(true); // 読み込み中フラグを立てる
@@ -29,6 +39,17 @@ const IndexPage: NextPage = () => {
 };
 
 export default IndexPage;
+
+// サーバーサイドで実行する処理
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
+
 // 型をImageとして定義
 type Image = { url: string; };
 const fetchImage = async (): Promise<Image> => {
