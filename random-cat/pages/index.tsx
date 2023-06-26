7680bd50f8a20bb052b6be76041f1a0048c73700
yuritakagi <takagiyuri0712@gmail.com>
2023-06-26 15:59:10 +0900

    add: 自動デプロイを体験

--- random-cat/pages/index.tsx
+++ random-cat/pages/index.tsx
@@ -25,9 +25,22 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
   };
   // ローディンク中でなければ画像を表示する
   return (
-    <div className={styles.frame}>
-      <button onClick={handleClick} className={styles.button}>他のにゃんこも見る</button>
-      <div>{loading || <img src={imageUrl} className={styles.img} />}</div>
+    <div>
+      <button
+        onClick={handleClick}
+        style={{
+          backgroundColor: "#319795",
+          border: "none",
+          borderRadius: "4px",
+          color: "white",
+          padding: "4px 8px"
+        }}
+      >
+        きょうのにゃんこ🐱
+      </button>
+      <div style={{ marginTop: 8, maxWidth: 500 }}>
+        <img src="{catImageUrl}" width="100%" height="auto" alt="猫" />
+      </div>
     </div>
   );
 };
